b188e0a35908ececf7c25cf3cbee67944f71602c
Derek Price <derekp098@gmail.com>
2020-11-26 19:30:43 -0800

    Made logo bigger. Recoded header from scratch. Made header more mobile friendly.

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,3 +145,19 @@
 	});
 
 })(jQuery);
+
+var prevScrollpos = window.pageYOffset;
+var screenWidth = document.documentElement.clientWidth;
+
+window.onscroll = function() {
+
+	if (screenWidth < 866) {
+		var currentScrollPos = window.pageYOffset;
+		if ((prevScrollpos > currentScrollPos)) {
+			document.getElementById("header").style.top = "0";
+		} else {
+			document.getElementById("header").style.top = "-80px";
+		}
+		prevScrollpos = currentScrollPos;
+	}
+}
